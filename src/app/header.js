672c83f85da2646720c4c3ea5e996c961dca2175
Randall Knutson <randall@form.io>
2017-06-13 15:05:07 -0500

    Replace existing starterkit with fountain.js built.

---
+++ src/app/header.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+const styles = {
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#1f1f1f'
+  },
+  title: {
+    flex: 1,
+    fontSize: '1.5rem',
+    margin: '1rem'
+  },
+  date: {
+    flex: 1,
+    textAlign: 'right',
+    margin: '1rem',
+    color: 'white'
+  }
+};
+
+export class Header extends Component {
+  render() {
+    return (
+      <header style={styles.header}>
+        <p style={styles.title}>
+          <a href="https://github.com/FountainJS/generator-fountain-webapp" target="_blank" rel="noopener noreferrer">
+            Fountain Generator
+          </a>
+        </p>
+        <p style={styles.date}>
+          Generated with FountainJS v1.0.0 on Tue Jun 13 2017 15:03:48 GMT-0500 (CDT)
+        </p>
+      </header>
+    );
+  }
+}
--- src/components/Router/constants.js
+++
@@ -1,2 +0,0 @@
-export const NAVIGATE = 'NAVIGATE';
-export const NOTHING = 'NOTHING';
