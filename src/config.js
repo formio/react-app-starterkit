6d3097ab188644475708eba7ae0796fd7fe9dd89
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2021-02-06 11:49:39 +0000

    Installed required dependencies and created a config

---
+++ src/config.js
@@ -0,0 +1,30 @@
+import { Formio } from 'react-formio';
+
+const getQuery = () => {
+  return window.location.search.substr(1).split('&').forEach(function(item) {
+    query[item.split('=')[0]] = item.split('=')[1] && decodeURIComponent(item.split('=')[1]);
+  });
+}
+
+const query = getQuery();
+const PROJECT_URL = query.projectUrl || 'https://reactstarter.form.io';
+const API_URL = query.apiUrl || 'https://api.form.io';
+
+export const AppConfig = {
+  projectUrl: PROJECT_URL,
+  apiUrl: API_URL
+};
+
+export const AuthConfig = {
+  anonState: '/auth',
+  authState: '/',
+  login: {
+    form: 'user/login'
+  },
+  register: {
+    form: 'user/register'
+  }
+};
+
+Formio.setProjectUrl(AppConfig.projectUrl);
+Formio.setBaseUrl(AppConfig.apiUrl);
