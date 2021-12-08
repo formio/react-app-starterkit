7994762b6c4496f321f4354c1b8c223acad26a22
TanyaGashtold <tanya@form.io>
2021-12-08 12:54:08 +0300

    fixed loading in IE11 + updated some dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
