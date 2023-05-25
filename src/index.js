480787f83822e66375a52ca9efd96b71a2fe051d
brendanjbond <brendanbond@gmail.com>
2023-05-25 12:04:08 -0500

    move to hashrouter for github pages SPA

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/react-app-starterkit">
-      <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
